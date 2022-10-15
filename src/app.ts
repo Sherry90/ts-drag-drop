a8793a3cfe8d52200ce579bf5b7434f56a95aadc
Sherry90 <hhlsdkm@gmail.com>
2022-10-16 01:08:06 +0900

    project render from class

--- src/app.ts
+++ src/app.ts
@@ -118,6 +118,28 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+// Project Item Class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
+  private project: Project;
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent()
+  }
+
+  configure() {}
+
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = this.project.people.toString();
+    this.element.querySelector('p')!.textContent = this.project.description;
+
+  }
+}
+
 // ProjectList Class
 class ProjectList extends Component<HTMLElement, HTMLDivElement>{
   assignedProjects: Project[];
@@ -150,11 +172,7 @@ class ProjectList extends Component<HTMLElement, HTMLDivElement>{
   private renderProjects() {
     const listEl = document.getElementById(`${this.type}-project-list`)! as HTMLUListElement;
     listEl.innerHTML = '';
-    for (const prjItem of this.assignedProjects) {
-      const listItem = document.createElement('li');
-      listItem.textContent = prjItem.title;
-      listEl.appendChild(listItem);
-    }
+    this.assignedProjects.map(prjItem => new ProjectItem(this.element.querySelector('ul')!.id, prjItem));
   }
 
 }
